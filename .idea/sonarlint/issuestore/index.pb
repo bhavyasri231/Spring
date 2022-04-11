bcac03b1e141de58ea015c4b2a51ebf0e6864442
Bhavyasri.Penta <bhavyasri.penta@zemosolabs.com>
2022-04-11 09:28:31 +0530

    Lombok

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+K
+src/main/java/Mountain.java,f/c/fcd7b56accce58ee82a53c31703547e11ffaadd4
---
+++ src/main/java/Mountain.java
@@ -0,0 +1,8 @@
+import lombok.Data;
+
+public @Data
+class Mountain {
+    private final String name;
+    private final double latitude,logitude;
+    private String country;
+}
